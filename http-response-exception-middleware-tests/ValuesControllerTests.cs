7f6e2829fac1db2b2e41e0a204e887a70fc2ffea
James Jones <james.russel.jones@gmail.com>
2019-02-28 21:54:16 -0500

    Create solution and unit test project

---
+++ http-response-exception-middleware-tests/ValuesControllerTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using http_response_exception_middleware.Controllers;
+using http_response_exception_middleware.Exceptions;
+
+namespace http_response_exception_middleware_tests
+{
+    [TestClass]
+    public class ValuesControllerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(BadRequestException))]
+        public void Get_ThrowsException()
+        {
+            var controller = new ValuesController();
+
+            controller.Get();
+        }
+    }
+}
